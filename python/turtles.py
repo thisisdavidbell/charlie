2fdb241d5cc2cdb03bb34659edfe9729bd18e87a
thisisdavidbell_charlie <thisisdavidbell@hotmail.com>
2026-09-05 15:37:22 +0100

    changed some things to turtles.py

--- python/turtles.py
+++ python/turtles.py
@@ -2,11 +2,13 @@ import tkinter
 window = tkinter.Tk()
 canvas = tkinter.Canvas(window, width=400, height=400)
 canvas.pack()
-controlscreen = canvas.create_rectangle(0, 0, 175, 400, fill="black")
-up = canvas.create_rectangle(50, 50, 150, 150, fill="blue")
-down = canvas.create_rectangle(50, 200, 150, 300, fill="red")
-player = canvas.create_rectangle(200, 200, 300, 300, fill="green")
+controlscreen = canvas.create_rectangle(0, 0, 100, 400, fill="black")
+up = canvas.create_rectangle(25, 25, 75, 75, fill="blue")
+down = canvas.create_rectangle(25, 50, 75, 75, fill="red")
+
+player = canvas.create_rectangle(100, 100, 150, 150, fill="green")
 canvas.tag_bind(up, "<Button-1>", lambda event: canvas.move(player, 0, -10))
 canvas.tag_bind(down, "<Button-1>", lambda event: canvas.move(player, 0, 10))
 canvas.focus_set()
+
 window.mainloop()
